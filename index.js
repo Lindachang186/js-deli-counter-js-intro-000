e201c0220ac0ec33039d6af69304a027ccb01e56
Linda Chang <lindachang186@gmail.com>
2018-09-10 13:44:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine){
   if(katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`
-  } else return "There is nobody waiting to be served!"
+  }
+  return "There is nobody waiting to be served!"
 }
